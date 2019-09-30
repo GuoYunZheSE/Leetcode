437c83948438979ee2a452723b80045f7c90feeb
GuoYunZheSE <guoyunzhe.se@gmail.com>
2019-09-30 23:33:10 +0200

    DP Tutorial

--- Sujet/Algorithm/DP/Tutorial/LNDS.py
+++ Sujet/Algorithm/DP/Tutorial/LNDS.py
@@ -1,3 +1,17 @@
 # topcoder.com/community/competitive-programming/tutorials/dynamic-programming-from-novice-to-advanced/
 # The Python Solution of Problem:Find the length of the longest non-decreasing sequence.
 
+class LNDS:
+    def run(self,nums:list):
+        res=[1]*len(nums)
+        for eachnum in range(0,len(nums)):
+            beforenum=0
+            while beforenum<eachnum:
+                if nums[beforenum]<=nums[eachnum]:
+                    res[eachnum] = max(res[eachnum], res[beforenum] + 1)
+                beforenum+=1
+        return max(res)
+
+if __name__ == '__main__':
+    sol=LNDS()
+    print(sol.run([5, 3, 4, 8, 6, 7,8]))
