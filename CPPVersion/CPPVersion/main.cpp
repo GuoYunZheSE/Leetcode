a0ccdf3a67a8e64c4b314dc55cf6930d9666b9c2
GuoYunZheSE <guoyunzhe.se@gmail.com>
2019-11-18 23:53:03 +0100

    finally

---
+++ CPPVersion/CPPVersion/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPPVersion
+//
+//  Created by 郭蕴喆 on 2019/11/18.
+//  Copyright © 2019 Lucas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
