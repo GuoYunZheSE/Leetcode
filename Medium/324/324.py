b0bd4a4a0d5ead3459824842ac6bcf411c482f77
GuoYunZheSE <guoyunzhe.se@gmail.com>
2020-02-15 23:59:32 +0100

    324 Y PY

---
+++ Medium/324/324.py
@@ -0,0 +1,23 @@
+class Solution:
+    def wiggleSort(self, nums: [int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums)<=1:
+            return nums
+        if len(nums)==2:
+            if nums[0]>nums[1]:
+                nums[0],nums[1]=nums[1],nums[0]
+            return nums
+        for i in range(0,len(nums)-1):
+            if i % 2==0:
+                if nums[i]>=nums[i+1]:
+                    nums[i],nums[i+1]=nums[i+1],nums[i]
+            else:
+                if nums[i]<=nums[i+1]:
+                    nums[i], nums[i + 1] = nums[i + 1], nums[i]
+        return nums
+if __name__ == '__main__':
+    nums=[1, 5, 1, 1, 6, 4]
+    S=Solution()
+    print(S.wiggleSort(nums))
