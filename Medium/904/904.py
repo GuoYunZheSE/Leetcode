36e1143bf32275993f8d47aa4a93dbcda1027d1c
GuoYunZheSE <guoyunzhe.se@gmail.com>
2020-02-28 22:25:37 +0100

    906

---
+++ Medium/904/904.py
@@ -0,0 +1,8 @@
+# @Date    : 21:41 02/28/2020
+# @Author  : ClassicalPi
+# @FileName: 904.py
+# @Software: PyCharm
+import typing
+class Solution:
+    def totalFruit(self, tree: typing.List[int]) -> int:
+        
