27eda127d9567d32cd0e29f10694a2ff72b1a101
GuoYunZheSE <guoyunzhe.se@gmail.com>
2020-02-29 02:07:09 +0100

    904 Y PY

--- Medium/904/904.py
+++ Medium/904/904.py
@@ -5,4 +5,22 @@
 import typing
 class Solution:
     def totalFruit(self, tree: typing.List[int]) -> int:
-        
+        basket={}
+        left,right=0,0
+        res=0
+        for right in range(0,len(tree)):
+            if basket.__contains__(tree[right]):
+                basket[tree[right]]+=1
+            else:
+                basket.setdefault(tree[right],1)
+            while len(basket)>2:
+                basket[tree[left]]-=1
+                if basket[tree[left]]==0:
+                     basket.pop(tree[left])
+                left+=1
+            res=max(res,right-left+1)
+        return res
+if __name__ == '__main__':
+    tree=[1,2,1]
+    s=Solution()
+    print(s.totalFruit(tree))
