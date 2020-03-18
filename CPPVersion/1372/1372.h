9282d640252c007b0f7f1176d7114d0156f9546d
GuoYunZheSE <guoyunzhe.se@gmail.com>
2020-03-19 00:49:35 +0100

    1372 Y CPP

---
+++ CPPVersion/1372/1372.h
@@ -0,0 +1,40 @@
+//
+// Created by 郭蕴喆 on 2020/3/19.
+//
+
+#ifndef CPPVERSION_1372_H
+#define CPPVERSION_1372_H
+
+#endif //CPPVERSION_1372_H
+
+#import <algorithm>
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ };
+
+class Solution {
+public:
+    int depth=0;
+    void DFS(TreeNode* root, bool last_is_left, int depth){
+        this->depth=std::max(depth,this->depth);
+        if(root== nullptr){
+            return;
+        } else{
+            if (last_is_left){
+                this->DFS(root->left, true,0);
+                this->DFS(root->right, false,depth+1);
+            } else{
+                this->DFS(root->right, false,0);
+                this->DFS(root->left, true,depth+1);
+            }
+        }
+    }
+    int longestZigZag(TreeNode* root) {
+        this->DFS(root->left, true,0);
+        this->DFS(root->right, false,0);
+        return this->depth;
+    }
+};
