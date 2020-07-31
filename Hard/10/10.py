bfbe0305ca45a6feaa5f969e80a815b509b479a8
GuoYunZheSE <guoyunzhe.se@gmail.com>
2020-07-31 23:33:17 +0800

    re * py

--- Hard/10/10.py
+++ Hard/10/10.py
@@ -3,13 +3,18 @@
 # @FileName: 10.py
 # @Software: PyCharm
 import collections
-class Solution:
-    def isMatch(self, s: str, p: str) -> bool:
-        
-
+class Solution(object):
+    def isMatch(self, text, pattern):
+        if not pattern:
+            return not text
+        first_match=bool(text) and pattern[0] in {'.',text[0]}
+        if len(pattern)>=2 and pattern[1]=='*':
+            return self.isMatch(text,pattern[2:]) or (first_match and self.isMatch(text[1:],pattern))
+        else:
+            return first_match and self.isMatch(text[1:],pattern[1:])
 if __name__ == '__main__':
     S=Solution()
-    print(S.isMatch("aa","a"))
+    print(S.isMatch("aa","a*"))
     print(S.isMatch("aa", "a*"))
     print(S.isMatch("ab", ".*"))
     print(S.isMatch("aa", "a"))
