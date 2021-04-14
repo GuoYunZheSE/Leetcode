d1abf2a6e643939abc223c092b43bcba3675467c
GuoYunZheSE <guoyunzhe.se@gmail.com>
2021-04-14 11:36:28 +0800

    139.py

--- Medium/139/139.py
+++ Medium/139/139.py
@@ -2,24 +2,30 @@
 # @Author  : ClassicalPi
 # @FileName: 139.py
 # @Software: PyCharm
+import timeit
 class Solution:
-    def __init__(self):
-        self.res=False
-        self.wordDict=set()
-    def DFS(self,s:str):
-        if s=="":
-            self.res=True
-        if self.res==True:
-            return
-        for i in range(0,len(s)+1):
-            if s[0:i] in self.wordDict:
-                self.DFS(s[i:])
+    # def __init__(self):
+    #     self.res=False
+    #     self.wordDict=set()
+    # def DFS(self,s:str):
+    #     if s=="":
+    #         self.res=True
+    #     if self.res==True:
+    #         return
+    #     for i in range(0,len(s)+1):
+    #         if s[0:i] in self.wordDict:
+    #             self.DFS(s[i:])
+    # def wordBreak(self, s: str, wordDict: [str]) -> bool:
+    #     self.wordDict=set(wordDict)
+    #     self.DFS(s)
+    #     return self.res
     def wordBreak(self, s: str, wordDict: [str]) -> bool:
-        self.wordDict=set(wordDict)
-        self.DFS(s)
-        return self.res
+        ans=[True]
+        self.wordDict = set(wordDict)
+        for i in range(1,len(s)+1):
+            ans+=any(ans[j] and s[j:i] in self.wordDict for j in range(i)),
+        return ans[-1]
 
 if __name__ == '__main__':
-    s=["leet", "code"]
-    S=Solution()
-    print(S.wordBreak("leetcode",s))
+    t = timeit.timeit(stmt='''wordBreak(s = "leetcode", wordDict = ["leet","code"])''', setup="from  __main__ import wordBreak", number=1000000)
+    print(t)
